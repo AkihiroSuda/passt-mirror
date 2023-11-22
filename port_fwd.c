AkihiroSuda/passt-mirror

commit 457ff122e33cf6a6e559b073f41c530e42d9c597

udp,pasta: Periodically scan for ports to automatically forward
pasta supports automatic port forwarding, where we look for listening
sockets in /proc/net (in both namespace and outside) and establish port
forwarding to match.

For TCP we do this scan both at initial startup, then periodically
thereafter.  For UDP however, we currently only scan at start.  So unlike
TCP we won't update forwarding to handle services that start after pasta
has begun.

There's no particular reason for that, other than that we didn't implement
it.  So, remove that difference, by scanning for new UDP forwards
periodically too.  The logic is basically identical to that for TCP, but it
needs some changes to handle the mildly different data structures in the
UDP case.

Link: https://bugs.passt.top/show_bug.cgi?id=45
Link: https://github.com/rootless-containers/rootlesskit/issues/383
Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
Signed-off-by: Stefano Brivio <sbrivio@redhat.com>

udp.c
@@ -1187,6 +1187,66 @@ static void udp_timer_one(struct ctx *c, int v6, enum udp_act_type type,
 }
 
 /**
+ * udp_port_rebind() - Rebind ports to match forward maps
+ * @c:		Execution context
+ * @outbound:	True to remap outbound forwards, otherwise inbound
+ *
+ * Must be called in namespace context if @outbound is true.
+ */
+static void udp_port_rebind(struct ctx *c, bool outbound)
+{
+	const uint8_t *fmap
+		= outbound ? c->udp.fwd_out.f.map : c->udp.fwd_in.f.map;
+	const uint8_t *rmap
+		= outbound ? c->udp.fwd_in.f.map : c->udp.fwd_out.f.map;
+	struct udp_splice_port (*socks)[NUM_PORTS]
+		= outbound ? udp_splice_ns : udp_splice_init;
+	unsigned port;
+
+	for (port = 0; port < NUM_PORTS; port++) {
+		if (!bitmap_isset(fmap, port)) {
+			if (socks[V4][port].sock >= 0) {
+				close(socks[V4][port].sock);
+				socks[V4][port].sock = -1;
+			}
+
+			if (socks[V6][port].sock >= 0) {
+				close(socks[V6][port].sock);
+				socks[V6][port].sock = -1;
+			}
+
+			continue;
+		}
+
+		/* Don't loop back our own ports */
+		if (bitmap_isset(rmap, port))
+			continue;
+
+		if ((c->ifi4 && socks[V4][port].sock == -1) ||
+		    (c->ifi6 && socks[V6][port].sock == -1))
+			udp_sock_init(c, outbound, AF_UNSPEC, NULL, NULL, port);
+	}
+}
+
+/**
+ * udp_port_rebind_outbound() - Rebind ports in namespace
+ * @arg:	Execution context
+ *
+ * Called with NS_CALL()
+ *
+ * Return: 0
+ */
+static int udp_port_rebind_outbound(void *arg)
+{
+	struct ctx *c = (struct ctx *)arg;
+
+	ns_enter(c);
+	udp_port_rebind(c, true);
+
+	return 0;
+}
+
+/**
  * udp_timer() - Scan activity bitmaps for ports with associated timed events
  * @c:		Execution context
  * @ts:		Timestamp from caller
@@ -1196,6 +1256,20 @@ void udp_timer(struct ctx *c, const struct timespec *ts)
 	int n, t, v6 = 0;
 	unsigned int i;
 	long *word, tmp;
+
+	if (c->mode == MODE_PASTA) {
+		if (c->udp.fwd_out.f.mode == FWD_AUTO) {
+			port_fwd_scan_udp(&c->udp.fwd_out.f, &c->udp.fwd_in.f,
+					  &c->tcp.fwd_out);
+			NS_CALL(udp_port_rebind_outbound, c);
+		}
+
+		if (c->udp.fwd_in.f.mode == FWD_AUTO) {
+			port_fwd_scan_udp(&c->udp.fwd_in.f, &c->udp.fwd_out.f,
+					  &c->tcp.fwd_in);
+			udp_port_rebind(c, false);
+		}
+	}
 
 	if (!c->ifi4)
 		v6 = 1;

passt.1
@@ -496,8 +496,7 @@ Default is \fBauto\fR.
 .BR \-u ", " \-\-udp-ports " " \fIspec
 Configure UDP port forwarding to namespace. \fIspec\fR is as described for TCP
 above, and the list of ports is derived from listening sockets reported by
-\fI/proc/net/udp\fR and \fI/proc/net/udp6\fR, see \fBproc\fR(5),
-when \fBpasta\fR starts (not periodically).
+\fI/proc/net/udp\fR and \fI/proc/net/udp6\fR, see \fBproc\fR(5).
 
 Note: unless overridden, UDP ports with numbers corresponding to forwarded TCP
 port numbers are forwarded too, without, however, any port translation. 

commit 4117bd94f967709074ff1f310d7334ab1876ddae

port_fwd, util: Don't bind UDP ports with opposite-side bound TCP ports
When pasta periodically scans bound ports and binds them on the other
side in order to forward traffic, we bind UDP ports for corresponding
TCP port numbers, too, to support protocols and applications such as
iperf3 which use UDP port numbers matching the ones used by the TCP
data connection.

If we scan UDP ports in order to bind UDP ports, we skip detection of
the UDP ports we already bound ourselves, to avoid looping back our
own ports. Same with scanning and binding TCP ports.

But if we scan for TCP ports in order to bind UDP ports, we need to
skip bound TCP ports too, otherwise, as David pointed out:

- we find a bound TCP port on side A, and bind the corresponding TCP
  and UDP ports on side B

- at the next periodic scan, we find that UDP port bound on side B,
  and we bind the corresponding UDP port on side A

- at this point, we unbind that UDP port on side B: we would
  otherwise loop back our own port.

To fix this, we need to avoid binding UDP ports that we already
bound, on the other side, as a consequence of finding a corresponding
bound TCP port.

Reproducing this issue is straightforward:

  ./pasta -- iperf3 -s

  # Wait one second, then from another terminal:
  iperf3 -c ::1 -u

Reported-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
Analysed-by: David Gibson <david@gibson.dropbear.id.au>
Fixes: 457ff122e33c ("udp,pasta: Periodically scan for ports to automatically forward")
Signed-off-by: Stefano Brivio <sbrivio@redhat.com>

port_fwd.c
@@ -86,22 +86,33 @@ void port_fwd_scan_tcp(struct port_fwd *fwd, const struct port_fwd *rev)
  * port_fwd_scan_tcp() - Scan /proc to update TCP forwarding map
  * @fwd:	Forwarding information to update
  * @rev:	Forwarding information for the reverse direction
- * @tcp:	Corresponding TCP forwarding information
+ * @tcp_fwd:	Corresponding TCP forwarding information
+ * @tcp_rev:	TCP forwarding information for the reverse direction
  */
 void port_fwd_scan_udp(struct port_fwd *fwd, const struct port_fwd *rev,
-		       const struct port_fwd *tcp)
+		       const struct port_fwd *tcp_fwd,
+		       const struct port_fwd *tcp_rev)
 {
+	uint8_t exclude[PORT_BITMAP_SIZE];
+
+	bitmap_or(exclude, PORT_BITMAP_SIZE, rev->map, tcp_rev->map);
+
 	memset(fwd->map, 0, PORT_BITMAP_SIZE);
-	procfs_scan_listen(fwd->scan4, UDP_LISTEN, fwd->map, rev->map);
-	procfs_scan_listen(fwd->scan6, UDP_LISTEN, fwd->map, rev->map);
+	procfs_scan_listen(fwd->scan4, UDP_LISTEN, fwd->map, exclude);
+	procfs_scan_listen(fwd->scan6, UDP_LISTEN, fwd->map, exclude);
 
 	/* Also forward UDP ports with the same numbers as bound TCP ports.
 	 * This is useful for a handful of protocols (e.g. iperf3) where a TCP
 	 * control port is used to set up transfers on a corresponding UDP
 	 * port.
+	 *
+	 * This means we need to skip numbers of TCP ports bound on the other
+	 * side, too. Otherwise, we would detect corresponding UDP ports as
+	 * bound and try to forward them from the opposite side, but it's
+	 * already us handling them.
 	 */
-	procfs_scan_listen(tcp->scan4, TCP_LISTEN, fwd->map, rev->map);
-	procfs_scan_listen(tcp->scan6, TCP_LISTEN, fwd->map, rev->map);
+	procfs_scan_listen(tcp_fwd->scan4, TCP_LISTEN, fwd->map, exclude);
+	procfs_scan_listen(tcp_fwd->scan6, TCP_LISTEN, fwd->map, exclude);
 }
 
 /**
@@ -126,7 +137,7 @@ void port_fwd_init(struct ctx *c)
 		c->udp.fwd_in.f.scan4 = open_in_ns(c, "/proc/net/udp", flags);
 		c->udp.fwd_in.f.scan6 = open_in_ns(c, "/proc/net/udp6", flags);
 		port_fwd_scan_udp(&c->udp.fwd_in.f, &c->udp.fwd_out.f,
-				  &c->tcp.fwd_in);
+				  &c->tcp.fwd_in, &c->tcp.fwd_out);
 	}
 	if (c->tcp.fwd_out.mode == FWD_AUTO) {
 		c->tcp.fwd_out.scan4 = open("/proc/net/tcp", flags);
@@ -137,6 +148,6 @@ void port_fwd_init(struct ctx *c)
 		c->udp.fwd_out.f.scan4 = open("/proc/net/udp", flags);
 		c->udp.fwd_out.f.scan6 = open("/proc/net/udp6", flags);
 		port_fwd_scan_udp(&c->udp.fwd_out.f, &c->udp.fwd_in.f,
-				  &c->tcp.fwd_out);
+				  &c->tcp.fwd_out, &c->tcp.fwd_in);
 	}
 }

port_fwd.h
@@ -37,7 +37,8 @@ struct port_fwd {
 
 void port_fwd_scan_tcp(struct port_fwd *fwd, const struct port_fwd *rev);
 void port_fwd_scan_udp(struct port_fwd *fwd, const struct port_fwd *rev,
-		       const struct port_fwd *tcp);
+		       const struct port_fwd *tcp_fwd,
+		       const struct port_fwd *tcp_rev);
 void port_fwd_init(struct ctx *c);
 
 #endif /* PORT_FWD_H */

udp.c
@@ -1260,13 +1260,13 @@ void udp_timer(struct ctx *c, const struct timespec *ts)
 	if (c->mode == MODE_PASTA) {
 		if (c->udp.fwd_out.f.mode == FWD_AUTO) {
 			port_fwd_scan_udp(&c->udp.fwd_out.f, &c->udp.fwd_in.f,
-					  &c->tcp.fwd_out);
+					  &c->tcp.fwd_out, &c->tcp.fwd_in);
 			NS_CALL(udp_port_rebind_outbound, c);
 		}
 
 		if (c->udp.fwd_in.f.mode == FWD_AUTO) {
 			port_fwd_scan_udp(&c->udp.fwd_in.f, &c->udp.fwd_out.f,
-					  &c->tcp.fwd_in);
+					  &c->tcp.fwd_in, &c->tcp.fwd_out);
 			udp_port_rebind(c, false);
 		}
 	}

util.c
@@ -325,6 +325,27 @@ int bitmap_isset(const uint8_t *map, int bit)
 	return !!(*word & BITMAP_BIT(bit));
 }
 
+/**
+ * bitmap_or() - Logical disjunction (OR) of two bitmaps
+ * @dst:	Pointer to result bitmap
+ * @size:	Size of bitmaps, in bytes
+ * @a:		First operand
+ * @b:		Second operand
+ */
+void bitmap_or(uint8_t *dst, size_t size, const uint8_t *a, const uint8_t *b)
+{
+	unsigned long *dw = (unsigned long *)dst;
+	unsigned long *aw = (unsigned long *)a;
+	unsigned long *bw = (unsigned long *)b;
+	size_t i;
+
+	for (i = 0; i < size / sizeof(long); i++, dw++, aw++, bw++)
+		*dw = *aw | *bw;
+
+	for (i = size / sizeof(long) * sizeof(long); i < size; i++)
+		dst[i] = a[i] | b[i];
+}
+
 /*
  * ns_enter() - Enter configured user (unless already joined) and network ns
  * @c:		Execution context

util.h
@@ -216,6 +216,7 @@ int timespec_diff_ms(const struct timespec *a, const struct timespec *b);
 void bitmap_set(uint8_t *map, int bit);
 void bitmap_clear(uint8_t *map, int bit);
 int bitmap_isset(const uint8_t *map, int bit);
+void bitmap_or(uint8_t *dst, size_t size, const uint8_t *a, const uint8_t *b);
 char *line_read(char *buf, size_t len, int fd);
 void ns_enter(const struct ctx *c);
 bool ns_is_init(void);