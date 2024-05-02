AkihiroSuda/passt-mirror

commit 6c7623d07bbd44d81beb4aa60b0a1de12b622630

netlink: Add support to fetch default gateway from multipath routes
If the default route for a given IP version is a multipath one,
instead of refusing to start because there's no RTA_GATEWAY attribute
in the set returned by the kernel, we can just pick one of the paths.

To make this somewhat less arbitrary, pick the path with the highest
weight, if weights differ.

Reported-by: Ed Santiago <santiago@redhat.com>
Link: https://github.com/containers/podman/issues/20927
Signed-off-by: Stefano Brivio <sbrivio@redhat.com>
Reviewed-by: David Gibson <david@gibson.dropbear.id.au>

netlink.c
@@ -271,16 +271,58 @@ unsigned int nl_get_ext_if(int s, sa_family_t af)
 
 		for (rta = RTM_RTA(rtm), na = RTM_PAYLOAD(nh); RTA_OK(rta, na);
 		     rta = RTA_NEXT(rta, na)) {
-			if (rta->rta_type != RTA_OIF)
-				continue;
+			if (rta->rta_type == RTA_OIF) {
+				ifi = *(unsigned int *)RTA_DATA(rta);
+			} else if (rta->rta_type == RTA_MULTIPATH) {
+				struct rtnexthop *rtnh;
 
-			ifi = *(unsigned int *)RTA_DATA(rta);
+				rtnh = (struct rtnexthop *)RTA_DATA(rta);
+				ifi = rtnh->rtnh_ifindex;
+			}
 		}
 	}
+
 	if (status < 0)
 		warn("netlink: RTM_GETROUTE failed: %s", strerror(-status));
 
 	return ifi;
+}
+
+/**
+ * nl_route_get_def_multipath() - Get lowest-weight route from nexthop list
+ * @rta:	Routing netlink attribute with type RTA_MULTIPATH
+ * @gw:		Default gateway to fill
+ *
+ * Return: true if a gateway was found, false otherwise
+ */
+bool nl_route_get_def_multipath(struct rtattr *rta, void *gw)
+{
+	struct rtnexthop *rtnh;
+	bool found = false;
+	int hops = -1;
+
+	for (rtnh = (struct rtnexthop *)RTA_DATA(rta);
+	     RTNH_OK(rtnh, RTA_PAYLOAD(rta)); rtnh = RTNH_NEXT(rtnh)) {
+		size_t len = rtnh->rtnh_len - sizeof(*rtnh);
+		struct rtattr *rta_inner;
+
+		if (rtnh->rtnh_hops < hops)
+			continue;
+
+		hops = rtnh->rtnh_hops;
+
+		for (rta_inner = RTNH_DATA(rtnh); RTA_OK(rta_inner, len);
+		     rta_inner = RTA_NEXT(rta_inner, len)) {
+
+			if (rta_inner->rta_type != RTA_GATEWAY)
+				continue;
+
+			memcpy(gw, RTA_DATA(rta_inner), RTA_PAYLOAD(rta_inner));
+			found = true;
+		}
+	}
+
+	return found;
 }
 
 /**
@@ -326,6 +368,9 @@ int nl_route_get_def(int s, unsigned int ifi, sa_family_t af, void *gw)
 
 		for (rta = RTM_RTA(rtm), na = RTM_PAYLOAD(nh); RTA_OK(rta, na);
 		     rta = RTA_NEXT(rta, na)) {
+			if (rta->rta_type == RTA_MULTIPATH)
+				found = nl_route_get_def_multipath(rta, gw);
+
 			if (rta->rta_type != RTA_GATEWAY)
 				continue;
 

passt.1
@@ -171,8 +171,10 @@ Assign IPv4 \fIaddr\fR as default gateway via DHCP (option 3), or IPv6
 \fIaddr\fR as source for NDP Router Advertisement and DHCPv6 messages.
 This option can be specified zero (for defaults) to two times (once for IPv4,
 once for IPv6).
-By default, IPv4 and IPv6 addresses are taken from the host interface with the
-first default route for the corresponding IP version.
+By default, IPv4 and IPv6 gateways are taken from the host interface with the
+first default route for the corresponding IP version. If the default route is a
+multipath one, the gateway is the first nexthop router returned by the kernel
+which has the highest weight in the set of paths.
 
 Note: these addresses are also used as source address for packets directed to
 the guest or to the target namespace having a loopback or local source address,

commit 76e32022c4085952eea119fa69976c8bccf2710c

netlink: Fix iterations over nexthop objects
Somewhat confusingly, RTNH_NEXT(), as defined by <linux/rtnetlink.h>,
doesn't take an attribute length parameter like RTA_NEXT() does, and
I just modelled loops over nexthops after RTA loops, forgetting to
decrease the remaining length we pass to RTNH_OK().

In practice, this didn't cause issue in any of the combinations I
checked, at least without the next patch.

We seem to be the only user of RTNH_OK(): even iproute2 has an
open-coded version of it in print_rta_multipath() (ip/iproute.c).

Introduce RTNH_NEXT_AND_DEC(), similar to RTA_NEXT(), and use it.

Fixes: 6c7623d07bbd ("netlink: Add support to fetch default gateway from multipath routes")
Fixes: f4e38b5cd232 ("netlink: Adjust interface index inside copied nexthop objects too")
Signed-off-by: Stefano Brivio <sbrivio@redhat.com>
Reviewed-by: David Gibson <david@gibson.dropbear.id.au>

netlink.c
@@ -36,6 +36,10 @@
 #include "ip.h"
 #include "netlink.h"
 
+/* Same as RTA_NEXT() but for nexthops: RTNH_NEXT() doesn't take 'attrlen' */
+#define RTNH_NEXT_AND_DEC(rtnh, attrlen)				\
+	((attrlen) -= RTNH_ALIGN((rtnh)->rtnh_len), RTNH_NEXT(rtnh))
+
 /* Netlink expects a buffer of at least 8kiB or the system page size,
  * whichever is larger.  32kiB is recommended for more efficient.
  * Since the largest page size on any remotely common Linux setup is
@@ -349,12 +353,13 @@ unsigned int nl_get_ext_if(int s, sa_family_t af)
  */
 bool nl_route_get_def_multipath(struct rtattr *rta, void *gw)
 {
+	size_t nh_len = RTA_PAYLOAD(rta);
 	struct rtnexthop *rtnh;
 	bool found = false;
 	int hops = -1;
 
 	for (rtnh = (struct rtnexthop *)RTA_DATA(rta);
-	     RTNH_OK(rtnh, RTA_PAYLOAD(rta)); rtnh = RTNH_NEXT(rtnh)) {
+	     RTNH_OK(rtnh, nh_len); rtnh = RTNH_NEXT_AND_DEC(rtnh, nh_len)) {
 		size_t len = rtnh->rtnh_len - sizeof(*rtnh);
 		struct rtattr *rta_inner;
 
@@ -566,11 +571,12 @@ int nl_route_dup(int s_src, unsigned int ifi_src,
 			if (rta->rta_type == RTA_OIF) {
 				*(unsigned int *)RTA_DATA(rta) = ifi_dst;
 			} else if (rta->rta_type == RTA_MULTIPATH) {
+				size_t nh_len = RTA_PAYLOAD(rta);
 				struct rtnexthop *rtnh;
 
 				for (rtnh = (struct rtnexthop *)RTA_DATA(rta);
-				     RTNH_OK(rtnh, RTA_PAYLOAD(rta));
-				     rtnh = RTNH_NEXT(rtnh))
+				     RTNH_OK(rtnh, nh_len);
+				     rtnh = RTNH_NEXT_AND_DEC(rtnh, nh_len))
 					rtnh->rtnh_ifindex = ifi_dst;
 			} else if (rta->rta_type == RTA_PREFSRC) {
 				/* Host routes might include a preferred source