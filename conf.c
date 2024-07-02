hmgle/passt

commit ceddcac74a6eafae8d959adcbfee17d4cae2c3a8

conf, tap: False "Buffer not null terminated" positives, CWE-170
Those strings are actually guaranteed to be NULL-terminated. Reported
by Coverity.

Signed-off-by: Stefano Brivio <sbrivio@redhat.com>

conf.c
@@ -1035,7 +1035,7 @@ void conf(struct ctx *c, int argc, char **argv)
 				usage(argv[0]);
 			}
 
-			ret = snprintf(c->sock_path, sizeof(c->sock_path), "%s",
+			ret = snprintf(c->sock_path, UNIX_SOCK_MAX - 1, "%s",
 				       optarg);
 			if (ret <= 0 || ret >= (int)sizeof(c->pcap)) {
 				err("Invalid socket path: %s", optarg);
@@ -1048,9 +1048,9 @@ void conf(struct ctx *c, int argc, char **argv)
 				usage(argv[0]);
 			}
 
-			ret = snprintf(c->pasta_ifn, sizeof(c->pasta_ifn), "%s",
+			ret = snprintf(c->pasta_ifn, IFNAMSIZ - 1, "%s",
 				       optarg);
-			if (ret <= 0 || ret >= (int)sizeof(c->pasta_ifn)) {
+			if (ret <= 0 || ret >= IFNAMSIZ - 1) {
 				err("Invalid interface name: %s", optarg);
 				usage(argv[0]);
 			}

tap.c
@@ -798,9 +798,9 @@ static void tap_sock_unix_init(struct ctx *c)
 		char *path = addr.sun_path;
 
 		if (*c->sock_path)
-			strncpy(path, c->sock_path, UNIX_PATH_MAX);
+			memcpy(path, c->sock_path, UNIX_PATH_MAX);
 		else
-			snprintf(path, UNIX_PATH_MAX, UNIX_SOCK_PATH, i);
+			snprintf(path, UNIX_PATH_MAX - 1, UNIX_SOCK_PATH, i);
 
 		ex = socket(AF_UNIX, SOCK_STREAM | SOCK_NONBLOCK, 0);
 		if (ex < 0) {
@@ -899,7 +899,7 @@ static int tap_ns_tun(void *arg)
 	int flags = O_RDWR | O_NONBLOCK | O_CLOEXEC;
 	struct ctx *c = (struct ctx *)arg;
 
-	strncpy(ifr.ifr_name, c->pasta_ifn, IFNAMSIZ);
+	memcpy(ifr.ifr_name, c->pasta_ifn, IFNAMSIZ);
 
 	if (ns_enter(c) ||
 	    (tun_ns_fd = open("/dev/net/tun", flags)) < 0 ||

AkihiroSuda/passt-mirror

commit b625ed5feebaac457311fe5299c3ee99dab5503a

conf: Use the right maximum buffer size for c->sock_path
UNIX_SOCK_MAX is the maximum number we'll append to the socket path
if we generate it automatically. If it's given on the command line,
it can be up to UNIX_PATH_MAX (including the terminating character)
long.

UNIX_SOCK_MAX happened to kind of fit because it's 100 (instead of
108).

Commit ceddcac74a6e ("conf, tap: False "Buffer not null terminated"
positives, CWE-170") fixed the wrong problem: the right fix for the
problem at hand was actually commit cc287af173ca ("conf: Fix
incorrect bounds checking for sock_path parameter").

Fixes: ceddcac74a6e ("conf, tap: False "Buffer not null terminated" positives, CWE-170")
Signed-off-by: Stefano Brivio <sbrivio@redhat.com>
Reviewed-by: David Gibson <david@gibson.dropbear.id.au>

conf.c
@@ -1398,7 +1398,7 @@ void conf(struct ctx *c, int argc, char **argv)
 			c->foreground = 1;
 			break;
 		case 's':
-			ret = snprintf(c->sock_path, UNIX_SOCK_MAX - 1, "%s",
+			ret = snprintf(c->sock_path, sizeof(c->sock_path), "%s",
 				       optarg);
 			if (ret <= 0 || ret >= (int)sizeof(c->sock_path))
 				die("Invalid socket path: %s", optarg);