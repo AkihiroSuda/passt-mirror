AkihiroSuda/passt-mirror

commit baccfb95ce0e30f64d052f710cd5fedc8c7426c1

conf: Stop parsing options at first non-option argument
Given that pasta supports specifying a command to be executed on the
command line, even without the usual -- separator as long as there's
no ambiguity, we shouldn't eat up options that are not meant for us.

Paul reports, for instance, that with:

  pasta --config-net ip -6 route

-6 is taken by pasta to mean --ipv6-only, and we execute 'ip route'.
That's because getopt_long(), by default, shuffles the argument list
to shift non-option arguments at the end.

Avoid that by adding '+' at the beginning of 'optstring'.

Reported-by: Paul Holzinger <pholzing@redhat.com>
Signed-off-by: Stefano Brivio <sbrivio@redhat.com>
Reviewed-by: David Gibson <david@gibson.dropbear.id.au>

conf.c
@@ -1253,9 +1253,9 @@ void conf(struct ctx *c, int argc, char **argv)
 	if (c->mode == MODE_PASTA) {
 		c->no_dhcp_dns = c->no_dhcp_dns_search = 1;
 		fwd_default = FWD_AUTO;
-		optstring = "dqfel:hF:I:p:P:m:a:n:M:g:i:o:D:S:46t:u:T:U:";
+		optstring = "+dqfel:hF:I:p:P:m:a:n:M:g:i:o:D:S:46t:u:T:U:";
 	} else {
-		optstring = "dqfel:hs:F:p:P:m:a:n:M:g:i:o:D:S:461t:u:";
+		optstring = "+dqfel:hs:F:p:P:m:a:n:M:g:i:o:D:S:461t:u:";
 	}
 
 	c->tcp.fwd_in.mode = c->tcp.fwd_out.mode = FWD_UNSET;

util.c
@@ -710,7 +710,7 @@ void close_open_files(int argc, char **argv)
 	int name, rc;
 
 	do {
-		name = getopt_long(argc, argv, ":F", optfd, NULL);
+		name = getopt_long(argc, argv, "+:F", optfd, NULL);
 
 		if (name == 'F') {
 			errno = 0;

commit f00ebda369844a8570591a9defd46f9e304a82ef

util: Don't stop on unrelated values when looking for --fd in close_open_files()
Seen with krun: we get a file descriptor via --fd, but we close it and
happily use the same number for TCP files.

The issue is that if we also get other options before --fd, with
arguments, getopt_long() stops parsing them because it sees them as
non-option values.

Use the - modifier at the beginning of optstring (before :, which is
needed to avoid printing errors) instead of +, which means we'll
continue parsing after finding unrelated option values, but
getopt_long() won't reorder them anyway: they'll be passed with option
value '1', which we can ignore.

By the way, we also need to add : after F in the optstring, so that
we're able to parse the option when given as short name as well.

Now that we change the parsing mode between close_open_files() and
conf(), we need to reset optind to 0, not to 1, whenever we call
getopt_long() again in conf(), so that the internal initialisation
of getopt_long() evaluating GNU extensions is re-triggered.

Link: https://github.com/slp/krun/issues/17#issuecomment-2294943828
Fixes: baccfb95ce0e ("conf: Stop parsing options at first non-option argument")
Fixes: 09603cab28f9 ("passt, util: Close any open file that the parent might have leaked")
Signed-off-by: Stefano Brivio <sbrivio@redhat.com>
Reviewed-by: David Gibson <david@gibson.dropbear.id.au>

conf.c
@@ -1312,7 +1312,7 @@ void conf(struct ctx *c, int argc, char **argv)
 	c->udp.fwd_in.mode = c->udp.fwd_out.mode = FWD_UNSET;
 	memcpy(c->our_tap_mac, MAC_OUR_LAA, ETH_ALEN);
 
-	optind = 1;
+	optind = 0;
 	do {
 		name = getopt_long(argc, argv, optstring, options, NULL);
 
@@ -1712,7 +1712,7 @@ void conf(struct ctx *c, int argc, char **argv)
 	 * settings)
 	 */
 	udp_portmap_clear();
-	optind = 1;
+	optind = 0;
 	do {
 		name = getopt_long(argc, argv, optstring, options, NULL);
 
@@ -1782,7 +1782,7 @@ void conf(struct ctx *c, int argc, char **argv)
 		nl_sock_init(c, true);
 
 	/* ...and outbound port options now that namespaces are set up. */
-	optind = 1;
+	optind = 0;
 	do {
 		name = getopt_long(argc, argv, optstring, options, NULL);
 

util.c
@@ -728,7 +728,7 @@ void close_open_files(int argc, char **argv)
 	int name, rc;
 
 	do {
-		name = getopt_long(argc, argv, "+:F", optfd, NULL);
+		name = getopt_long(argc, argv, "-:F:", optfd, NULL);
 
 		if (name == 'F') {
 			errno = 0;